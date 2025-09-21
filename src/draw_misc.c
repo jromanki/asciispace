714246183d985df365c06f95f54b6e67ccb296a9
jromanki <jromanki03@gmail.com>
2025-09-22 00:00:34 +0200

    game loop

--- src/draw_misc.c
+++ src/draw_misc.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <curses.h>
+#include <string.h>
 
 void draw_line_pixels(int y0 ,int x0, int y1, int x1, char sign){
     // a'la bresenham algorithm
@@ -25,4 +26,11 @@ void draw_borders(int y_0 ,int x_0, int y_max, int x_max) {
     draw_line_pixels(y_0, x_max, y_max, x_max, '|');   // right
     draw_line_pixels(y_0, x_0, y_0, x_max, '-');       // top
     draw_line_pixels(y_max, x_max, y_max, x_0, '-');   // bottom
+}
+
+void you_lost_text(int x_size, int y_size){
+    char loose_str[] = "You lost!";
+    char again_str[] = "Press any key to play again.";
+    mvaddstr(y_size/2-1, x_size/2-strlen(loose_str)/2, loose_str);
+    mvaddstr(y_size/2, x_size/2-strlen(again_str)/2, again_str);
 }
--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 #define SPEED_FACTOR 0.14f
 #define ANGULAR_LIMIT 0.1f
 
-bool check_loose(Shape* s, int x_size, int y_size) {
+bool check_border_collision(Shape* s, int x_size, int y_size) {
     for (int i = 0; i < s->vertex_count; i++){
         if (s->vertices[i].x <= 0 || s->vertices[i].x >= x_size/2){
             return true;
@@ -25,68 +25,82 @@ bool check_loose(Shape* s, int x_size, int y_size) {
 }
 
 int main() {
-    int y_size; int x_size;
     WINDOW* win = initscr();
+    int y_size; int x_size;
     getmaxyx(win, y_size, x_size);
     x_size = x_size - 1; y_size = y_size - 1;
     curs_set(0);
     keypad(win, true);
     nodelay(win, true);
     srand(time(NULL)); // seed the rng with current time
-
-    Shape ship;
-    ship.vertex_count = 3;
-    ship.vertices = malloc(ship.vertex_count * sizeof(Point));
-    ship.vertices[0] = (Point){0, -8};
-    ship.vertices[1] = (Point){3, 2};
-    ship.vertices[2] = (Point){-3, 2};
-    ship.center = (Point){0, 0};
-    ship.facing = (Vector){0, -1};
-
     int x_start = x_size / 2 / 2;
     int y_start = y_size / 2;
-    translate(&ship, (float) x_start, (float) y_start);
-    float dx = 0;
-    float dy = 0;
-    float angular_vel = 0;
-    bool playing = true;
 
-    while (playing) {
-        int pressed = wgetch(win);
+    while(true){
+        Shape ship;
+        ship.vertex_count = 3;
+        ship.vertices = malloc(ship.vertex_count * sizeof(Point));
+        ship.vertices[0] = (Point){0, -8};
+        ship.vertices[1] = (Point){3, 2};
+        ship.vertices[2] = (Point){-3, 2};
+        ship.center = (Point){0, 0};
+        ship.facing = (Vector){0, -1};
 
-        if (pressed != ERR) {
-            if (pressed == KEY_LEFT){
-                if (angular_vel > - ANGULAR_LIMIT) angular_vel -= 0.01;
+        translate(&ship, (float) x_start, (float) y_start);
+        float dx = 0;
+        float dy = 0;
+        float angular_vel = 0;
+        bool playing = true;
+
+        while (playing) {
+            int pressed = wgetch(win);
+
+            if (pressed != ERR) {
+                if (pressed == KEY_LEFT){
+                    if (angular_vel > - ANGULAR_LIMIT) angular_vel -= 0.01;
+                }
+                if (pressed == KEY_RIGHT) {
+                    if (angular_vel < ANGULAR_LIMIT) angular_vel += 0.01;
+                }
+                if (pressed == KEY_UP){
+                    dx += ship.facing.x * SPEED_FACTOR;
+                    dy += ship.facing.y * SPEED_FACTOR;
+                }
+                if (pressed == 'q'){
+                    playing = false;
+                }
             }
-            if (pressed == KEY_RIGHT) {
-                if (angular_vel < ANGULAR_LIMIT) angular_vel += 0.01;
+
+            float current_x = ship.center.x;
+            float current_y = ship.center.y;
+            translate(&ship, -current_x, -current_y);
+            rotate(&ship, angular_vel);
+            translate(&ship, current_x + dx, current_y + dy);
+
+            erase();
+            draw_shape(&ship);
+            draw_borders(0, 0, y_size, x_size);
+            
+            refresh();
+
+            if (check_border_collision(&ship, x_size, y_size) == true){
+                playing = false;
             }
-            if (pressed == KEY_UP){
-                dx += ship.facing.x * SPEED_FACTOR;
-                dy += ship.facing.y * SPEED_FACTOR;
+
+            usleep(1000000 / FPS);
+        }
+
+        you_lost_text(x_size, y_size);
+        while (true){
+            int pressed = wgetch(win);
+            if (pressed != ERR){
+                break;
             }
         }
 
-        float current_x = ship.center.x;
-        float current_y = ship.center.y;
-        translate(&ship, -current_x, -current_y);
-        rotate(&ship, angular_vel);
-        translate(&ship, current_x + dx, current_y + dy);
-
-        erase();
-        draw_shape(&ship);
-        draw_borders(0, 0, y_size, x_size);
-        
-        refresh();
-
-        if (check_loose(&ship, x_size, y_size) == true){
-            playing = false;
-        }
-
-        usleep(1000000 / FPS);
+        free(ship.vertices);
     }
 
-    while (true){}
     endwin();
 
     return 0;
